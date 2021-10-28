8320d6019f6ff6679fc3c4600878d18cdc06ecb3
haswell <haswell@company.com>
2021-10-28 15:37:33 +0300

    add mainpage with image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/footer/Footer.component';
 import Header from './components/header/Header.component';
+import MainPage from './components/mainPage/MainPage.component';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainPage/>
       <Footer />
     </div>
   );
---
+++ src/components/mainPage/MainPage.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MainPage.component.css';
+
+export default function MainPage() {
+    return (
+        <div className="main-image-container">
+            <span>Lorem ipsum dolor sit <br/>
+                amet consectetur.</span>
+                <br/>
+            <button className="contact-btn">cвязаться</button>
+        </div>
+    )
+}
