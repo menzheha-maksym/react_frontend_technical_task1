d5406d12f2eb5652f9f7c5d388da5cf2fd893887
haswell <haswell@company.com>
2021-10-29 16:12:18 +0300

    add contact-modal

---
+++ src/components/contactModal/ContactModal.component.jsx
@@ -0,0 +1,71 @@
+import { faEnvelope, faPhoneAlt, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
+import Modal from 'react-modal';
+import './ContactModal.component.css';
+
+export default function ContactModal() {
+
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+
+    return (
+        <div>
+            <div>
+                <a className="contact" href="#4" onClick={openModal}><i><FontAwesomeIcon icon={faEnvelope} /></i>info@devstudio.com</a>
+
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Example Modal"
+                    className="modal"
+                >
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <span onClick={closeModal} class="close">&times;</span>
+                            <h2>Оставьте свои данные мы вам перезвоним</h2>
+                        </div>
+                        <div class="modal-body">
+                            <div className="modal-input-form">
+                                <span>Имя</span>
+                                <i className="user-icon"><FontAwesomeIcon icon={faUser}/></i>
+                                <input type="text" name="name" className="modal-text-input" placeholder="" />
+                            </div>
+                            <div className="modal-input-form">
+                                <span>Телефон</span>
+                                <i className="phone-icon"><FontAwesomeIcon icon={faPhoneAlt}/></i>
+                                <input type="text" name="phone" className="modal-text-input" placeholder="" />
+                            </div>
+                            <div className="modal-input-form">
+                                <span>Почта</span>
+                                <i className="email-icon"><FontAwesomeIcon icon={faEnvelope}/></i>
+                                <input type="email" name="email" className="modal-text-input" placeholder="" />
+                            </div>
+                            <div className="modal-input-form">
+                                <span>Коментарий</span>
+                                <textarea rows="6" placeholder='Введите текст' />
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <label className="label" >
+                                <input type="checkbox" value="yes" />
+                                Соглашаюсь с рассылкой и принимаю <span>Условия договора</span>
+                            </label>
+                            <br />
+                            <button className="send-btn">Отправить</button>
+                        </div>
+                    </div>
+                </Modal>
+            </div>
+        </div>
+    )
+}
--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -1,6 +1,7 @@
-import { faEnvelope, faMobile } from '@fortawesome/free-solid-svg-icons';
+import { faMobile } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
+import ContactModal from '../contactModal/ContactModal.component';
 import './Header.component.css';
 
 export default function Header() {
@@ -13,7 +14,8 @@ export default function Header() {
                 <a className="nav-link nav-link-grow-up" href="#3">Контакты</a>
             </div>
             <div className="contacts">
-                <a className="contact" href="#4"><i><FontAwesomeIcon icon={faEnvelope}/></i>info@devstudio.com</a>
+                <ContactModal />
+                {/* <a className="contact" href="#4"><i><FontAwesomeIcon icon={faEnvelope}/></i>info@devstudio.com</a> */}
                 <a className="contact" href="#5"><i><FontAwesomeIcon icon={faMobile}/></i>+38 096 111 11 11</a>
             </div>
         </div>
